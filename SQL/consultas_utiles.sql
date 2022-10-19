0354a2ab712f8134174f3ce54f3917b3f688f283
Matías David Schwerdt <schwerdt.matias@gmail.com>
2022-10-18 21:21:03 -0300

    agregue consultas utiles

--- SQL/consultas_utiles.sql
+++ SQL/consultas_utiles.sql
@@ -1,22 +1,31 @@
 # fecha mas reciente
-SELECT 
-	MAX(fecha) AS fecha_mas_reciente
-FROM 
-	jugadores;
+SELECT MAX(fecha) AS fecha_mas_reciente
+FROM jugadores;
 
 
-# ultima vez que se registró al club
-SELECT
-	fecha AS fecha_mas_reciente,
-	hora AS hora_mas_reciente, 
-	tag, 
-	copas 
-FROM 
-	jugadores
+# Elimina todas las filas cuya hora no es a las 00hs
+DELETE FROM jugadores
+WHERE HOUR(hora) NOT IN (00);
+
+
+# Tabla con ultimo registro
+SELECT *
+FROM jugadores
+WHERE fecha = (	SELECT MAX(fecha)
+				FROM jugadores)
+ORDER BY copas DESC;
+
+
+# Tabla con registro de hace 7 dias
+SELECT *
+FROM jugadores
+WHERE fecha = (SELECT DATE(NOW()) - INTERVAL 7 DAY)
+ORDER BY copas DESC;
+
+
+
+
+# diff de copas - FALTA TERMINAR
+SELECT j_7dias.fecha, j_actual.tag, (j_actual.copas - j_7dias.copas) AS copas
+FROM jugadores AS j_7dias, jugadores AS j_actual;
 WHERE
-	fecha = (	SELECT MAX(fecha)
-				FROM jugadores
-			)
-ORDER BY
-	copas 
-DESC;
