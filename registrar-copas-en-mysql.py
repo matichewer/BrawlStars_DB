234fa94db7ad742ebe29eb800fbf873da92fc363
Matías David Schwerdt <schwerdt.matias@gmail.com>
2022-10-25 13:16:51 -0300

    implements logs

--- registrar-copas-en-mysql.py
+++ registrar-copas-en-mysql.py
@@ -1,15 +1,22 @@
+from subprocess import _TXT
 import brawlstats
 import mysql.connector
 import requests
+import logging as log
 
-def bot_send_text(bot_message):
-    
+def bot_send_text(bot_message):    
     bot_token = '1744210955:AAHgEC8e24vuP2LNyrdT5c-AdoLd7AXtM7E'
     bot_chatID = '70429625'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-
     response = requests.get(send_text)
+	log.info('Telegram bot response: ' + response)
     return response
+
+log.basicConfig(filename='registrar-copas-en-mysql.log', 
+                    encoding='utf-8', 
+                    level=log.DEBUG,
+                    format='%(asctime)s, %(levelname)-8s, [%(filename)s:%(funcName)s:%(lineno)d], %(message)s',
+                    datefmt='%Y-%m-%d, %H:%M:%S')
 
 
 # Token Brawl Stars
@@ -17,8 +24,9 @@ client = ''
 try:
 	client = brawlstats.Client('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjIyNmYyOTc2LTBhYzgtNDE0MC05NTMyLTdhMmQzY2ZlY2ZlNCIsImlhdCI6MTY2NTI2MzE1OCwic3ViIjoiZGV2ZWxvcGVyL2ZlNGU4OWJiLTI5ZjMtMjFiOC05YzBmLTc2MzhhNGMxNmMzNiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTUyLjE2OS40Ny4xNjIiXSwidHlwZSI6ImNsaWVudCJ9XX0.gzk2ZKillueFmsvShvEiNjqR3zkj2p09Fqsw7ufqJ_ruJpkMtDeZ7XVQD3ybg2Gs5cSoBZEax7wB5kidyd9XrQ')
 except: # catch all exceptions
-	bot_send_text("Error making request to BrawlStars")
-	print("Error making request to BrawlStars")
+	msg = "Error making request to BrawlStars"
+	log.critical(msg)
+	bot_send_text(msg)
 	exit(1)
 	
 club = client.get_club('2GVC0QRPY')
@@ -42,4 +50,4 @@ mydb.commit()
 cursor.close()
 mydb.close()
 
-bot_send_text("BS: registro exitoso")
+log.info('Registro exitoso')
